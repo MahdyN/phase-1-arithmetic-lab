f9cf293ecad90897d84fc4e8ac94552789e6bc30
Mahdy Noureddine <mahdy.a.noureddine@gmail.com>
2021-11-22 18:23:06 -0500

    commit tree f82e0155e8ba3de88711d69d08fb7bc50c39b6ee

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1=31;
+const num2=2;
+const multiply=num1*num2;
 
-// Write your code here
+let random=Math.floor(Math.random()*10)+1;
+
+const num3=16;
+const num4=4;
+const mod=num3/num4;
+
+const max=Math.max(1,2,3,4,6,5,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
